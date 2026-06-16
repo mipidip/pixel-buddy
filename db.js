b2bf92b95c5498285c6a96abd7aa49c55393a5e6
mipidip <sumeyyeelifherdem@gmail.com>
2026-06-16 18:39:17 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,22 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Veri tabanına bağlanırken hata oluştu:', err.stack);
+  }
+  console.log('🚀 Pixel Buddy veri tabanına başarıyla bağlanıldı!');
+  release();
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+};
